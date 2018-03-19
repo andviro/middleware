ebe1bfa229621f1f3e9a629b4bbbd9f83b2d7e5b
Andrew Rodionoff <andviro@gmail.com>
2018-03-19 09:42:35 +0300

    added Push and Pop middleware

--- state/state.go
+++ state/state.go
@@ -18,41 +18,66 @@ const stateKey stateKeyType = iota
 type State struct {
 	Key   string
 	Value interface{}
+	Prev  *State
 }
 
-func (s State) Error() string {
+func (s *State) Error() string {
 	return s.Key
 }
 
 // Store saves and loads state
 type Store interface {
-	Get() (State, error) // read current state value
-	Set(State) error     // store state value
+	Get() (*State, error) // read current state value
+	Set(*State) error     // store state value
 }
 
 // Current extracts current state from the context
-func Current(ctx context.Context) (res State) {
-	res, _ = ctx.Value(stateKey).(State)
+func Current(ctx context.Context) (res *State) {
+	res, _ = ctx.Value(stateKey).(*State)
+	return
+}
+
+func lazy(val interface{}) (f func(context.Context) error) {
+	switch t := val.(type) {
+	case string:
+		f = func(context.Context) error { return &State{Key: t} }
+	case func(context.Context) error:
+		f = t
+	case State:
+		f = func(context.Context) error { return &t }
+	}
 	return
 }
 
 // Next returns next state or error from handler
 func Next(val interface{}) middleware.Middleware {
-	var f func(context.Context) State
-	switch t := val.(type) {
-	case string:
-		f = func(context.Context) State { return State{Key: t} }
-	case func(context.Context) State:
-		f = t
-	case State:
-		f = func(context.Context) State { return t }
-	}
+	f := lazy(val)
 	return func(ctx context.Context, next middleware.Handler) (err error) {
 		if err = next.Apply(ctx); err != nil {
 			return
 		}
 		return f(ctx)
 	}
+}
+
+// Push pushes state down on stack
+func Push(val interface{}) middleware.Middleware {
+	f := lazy(val)
+	return Next(func(ctx context.Context) error {
+		prev := Current(ctx)
+		next, ok := f(ctx).(*State)
+		if ok {
+			next.Prev = prev
+		}
+		return next
+	})
+}
+
+// Pop pops state from stack
+func Pop() middleware.Middleware {
+	return Next(func(ctx context.Context) error {
+		return Current(ctx).Prev
+	})
 }
 
 // With injects state into context
@@ -73,7 +98,7 @@ func Machine(factory func(context.Context) Store) middleware.Middleware {
 			return
 		}
 		err = next(context.WithValue(ctx, stateKey, st))
-		if nextState, ok := err.(State); ok {
+		if nextState, ok := err.(*State); ok {
 			return store.Set(nextState)
 		}
 		return
@@ -83,17 +108,17 @@ func Machine(factory func(context.Context) Store) middleware.Middleware {
 // Match constructs predicate that matches specified state, state name or
 // regular expression
 func Match(target interface{}) middleware.Predicate {
-	f := func(State) bool { return false }
+	f := func(*State) bool { return false }
 	switch t := target.(type) {
 	case string:
-		f = func(st State) bool { return t == st.Key }
-	case State:
-		f = func(st State) bool { return reflect.DeepEqual(t, st) }
+		f = func(st *State) bool { return t == st.Key }
+	case *State:
+		f = func(st *State) bool { return reflect.DeepEqual(t, st) }
 	case regexp.Regexp:
-		f = func(st State) bool { return t.MatchString(st.Key) }
+		f = func(st *State) bool { return t.MatchString(st.Key) }
 	}
 	return func(ctx context.Context) bool {
-		st, ok := ctx.Value(stateKey).(State)
+		st, ok := ctx.Value(stateKey).(*State)
 		return ok && f(st)
 	}
 }
